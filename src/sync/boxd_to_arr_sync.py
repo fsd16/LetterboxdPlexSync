3ca8c3a079d9debc9f524900616a885ca5bc5dd6
Finn Drabsch <fsd16@uclive.ac.nz>
2024-05-16 23:29:47 +1200

    Working sync

--- src/sync/boxd_to_arr_sync.py
+++ src/sync/boxd_to_arr_sync.py
@@ -1,50 +1,62 @@
 import logging
 from pathlib import Path
 
-from src.sync.lib.letterboxd import LetterBoxd
-from src.sync.lib.overseerr import Overseerr
-from dotenv import load_dotenv
+from .lib.letterboxd import LetterBoxd
+from .lib.overseerr import Overseerr
+from dotenv import load_dotenv, find_dotenv
 from getenv import Env
+from argparse import ArgumentParser
 
 LOG = logging.getLogger(__name__)
 
-env_path = Path("../../.env")
 
-load_dotenv(env_path)
-
-Env.set_prefix("OVERSEERR")
-OVERSEERR_HOST = Env("{prefix}_HOST")
-OVERSEERR_API_KEY = Env("{prefix}_API_KEY")
-
-Env.set_prefix("LBXD")
-LBXD_USERNAME = Env("{prefix}_USERNAME")
-LBXD_PASSWORD = Env("{prefix}_PASSWORD")
-
-if __name__ == "__main__":
+def app():
     LOG.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
     LOG.addHandler(handler)
 
-    LetterBoxd.username = LBXD_USERNAME.get()
-    LetterBoxd.password = LBXD_PASSWORD.get()
+    parser = ArgumentParser("BoxdArrSync", "Sync Letterboxd watchlist with Overseerr request list")
+    parser.add_argument("-e", "--env", help="Path to .env")
 
-    with LetterBoxd() as scrape:
+    args = parser.parse_args()
 
-        wl = scrape.get_watchlist()
+    if args.env:
+        env_path = Path(args.env).resolve()
+    else:
+        env_path = find_dotenv(raise_error_if_not_found=True)
+
+    load_dotenv(env_path)
+    LOG.debug(f"Environment variables loaded from: {env_path}")
+
+    Env.set_prefix("OVERSEERR")
+    overseerr_host = Env("{prefix}_HOST")
+    overseerr_api_key = Env("{prefix}_API_KEY")
+
+    Env.set_prefix("LBXD")
+    lbxd_username = Env("{prefix}_USERNAME")
+    lbxd_password = Env("{prefix}_PASSWORD")
+
+    LetterBoxd.username = lbxd_username.get()
+    LetterBoxd.password = lbxd_password.get()
+
+    with LetterBoxd() as boxd:
+
+        wl = boxd.get_watchlist()
 
         ids = []
         for url in wl["Letterboxd URI"]:
-            ids.append(int(scrape.get_tmdbid(url)))
+            tmdb_id = boxd.get_tmdbid(url)
+            if tmdb_id:
+                ids.append(int(tmdb_id))
+            else:
+                LOG.error(f"Failed to get TMDB ID for {url}")
 
-    overseerr = Overseerr(OVERSEERR_HOST.get(), OVERSEERR_API_KEY.get())
+    overseerr = Overseerr(overseerr_host.get(), overseerr_api_key.get())
 
     for tmdb_id in ids:
-        print(overseerr.request(tmdb_id))
-        break
+        overseerr.request(tmdb_id)
 
-    # movies = overseerr.media()
-    #
-    # ids = [m["tmdbId"] for m in movies if m["mediaType"] == "movie"]
-    # print(ids)
-    # print(len(ids))
+
+if __name__ == "__main__":
+    app()
